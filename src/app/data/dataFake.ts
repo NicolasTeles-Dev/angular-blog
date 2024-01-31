1eb9e3f73fb8ffddae643d44911b3ee66c66d843
nicolas <nicolas.gpteles@gmail.com>
2024-01-30 23:03:52 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "image": "https://www.zup.com.br/wp-content/uploads/2021/03/5e32dc5538c14b0d4b28f87f_imagens-angular3-1024x677.png",
+        "title": "Angular > React",
+        "description": "Angular espanca react e react pede para sair do mercado e diz assim: 'Errei em achar que acabaria com o Angular'"
+    },
+    {
+        "id":"2",
+        "image": "aaa",
+        "title": "saiu novo filme",
+        "description": "ola"
+    }
+];
